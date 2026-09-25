e07d0a2fffda57781d52306c47fc44620b0760a9
Knirl <amboymacrian@gmail.com>
2026-09-25 20:07:28 +0800

    Terraform fmt check.

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
 
 
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.65"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.65"
     }
+  }
 
-    required_version = "~> 1.16"
+  required_version = "~> 1.16"
 
 
 }
