6a7d93906d5cc5bab4eb37bb3c95db530a839939
Knirl <amboymacrian@gmail.com>
2026-09-26 10:33:39 +0800

    Cleanup: Remove all infrastructure resources

--- main.tf
+++ main.tf
@@ -11,34 +11,34 @@
 # IAM Role & Instance Profile: Grants the EC2 instance read-only access to an S3 bucket without embedding hardcoded AWS credentials in the code.
 
 # 1. Instantiate Networking Module
-module "vpc" {
-  source             = "./modules/vpc"
-  vpc_cidr           = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
-  environment        = var.environment
-}
+# module "vpc" {
+#   source             = "./modules/vpc"
+#   vpc_cidr           = var.vpc_cidr
+#   public_subnet_cidr = var.public_subnet_cidr
+#   environment        = var.environment
+# }
 
-# 2. Instantiate Security Module (Passes VPC ID from Networking Module)
-module "security" {
-  source      = "./modules/security"
-  vpc_id      = module.vpc.vpc_id
-  environment = var.environment
-}
+# # 2. Instantiate Security Module (Passes VPC ID from Networking Module)
+# module "security" {
+#   source      = "./modules/security"
+#   vpc_id      = module.vpc.vpc_id
+#   environment = var.environment
+# }
 
-# 3. Instantiate Compute Module (Passes Subnet & SG IDs from preceding modules)
-module "compute" {
-  source            = "./modules/compute"
-  subnet_id         = module.vpc.public_subnet_id
-  security_group_id = module.security.web_security_group_id
-  instance_type     = var.instance_type
-  environment       = var.environment
-}
+# # 3. Instantiate Compute Module (Passes Subnet & SG IDs from preceding modules)
+# module "compute" {
+#   source            = "./modules/compute"
+#   subnet_id         = module.vpc.public_subnet_id
+#   security_group_id = module.security.web_security_group_id
+#   instance_type     = var.instance_type
+#   environment       = var.environment
+# }
 
-# Testing the Pipeline - Adding a new resource - S3 Bucket
-resource "aws_s3_bucket" "pipeline_test_bucket" {
-  bucket_prefix = "cicd-test-bucket-"
+# # Testing the Pipeline - Adding a new resource - S3 Bucket
+# resource "aws_s3_bucket" "pipeline_test_bucket" {
+#   bucket_prefix = "cicd-test-bucket-"
 
-  tags = {
-    ManagedBy = "Terraform-CodePipeline"
-  }
-}
+#   tags = {
+#     ManagedBy = "Terraform-CodePipeline"
+#   }
+# }
