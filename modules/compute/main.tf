e3e7c0e61a0bee110c05a34b9e7d27054c936909
Knirl <amboymacrian@gmail.com>
2026-09-26 10:51:25 +0800

    restore github repo files.

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -15,23 +15,23 @@ data "aws_ami" "amazon_linux_2023" {
 }
 
 # 2. Use that dynamic AMI in your instance
-# resource "aws_instance" "this" {
-#   ami                         = data.aws_ami.amazon_linux_2023.id
-#   instance_type               = var.instance_type
-#   subnet_id                   = var.subnet_id
-#   vpc_security_group_ids      = [var.security_group_id]
-#   associate_public_ip_address = true
+resource "aws_instance" "this" {
+  ami                         = data.aws_ami.amazon_linux_2023.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  associate_public_ip_address = true
 
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               dnf update -y
-#               dnf install -y nginx
-#               systemctl enable nginx
-#               systemctl start nginx
-#               echo "<h1>Deployed via Modular Terraform Pipeline</h1>" > /usr/share/nginx/html/index.html
-#               EOF
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              echo "<h1>Deployed via Modular Terraform Pipeline</h1>" > /usr/share/nginx/html/index.html
+              EOF
 
-#   tags = {
-#     Name = "${var.environment}-web-server"
-#   }
-# }
+  tags = {
+    Name = "${var.environment}-web-server"
+  }
+}
